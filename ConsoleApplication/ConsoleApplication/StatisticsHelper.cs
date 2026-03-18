48f12106ed6b7ac028acfb19cfe1a33bd12d2c45
Jakub Fudala <s33046@pjwstk.edu.pl>
2026-03-18 18:35:28 +0100

    Add StatisticsHelper class

---
+++ ConsoleApplication/ConsoleApplication/StatisticsHelper.cs
@@ -0,0 +1,15 @@
+﻿namespace ConsoleApplication
+{
+    public static class StatisticsHelper
+    {
+        public static int Sum(int[] values)
+        {
+            int sum = 0;
+            foreach (var v in values)
+            {
+                sum += v;
+            }
+            return sum;
+        }
+    }
+}
