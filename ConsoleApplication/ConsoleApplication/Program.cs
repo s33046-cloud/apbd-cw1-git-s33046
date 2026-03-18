cc53b6c22c83787fe9a52bd1e02a073a457050f2
Jakub Fudala <s33046@pjwstk.edu.pl>
2026-03-18 18:43:25 +0100

    Display calculated sum

--- ConsoleApplication/ConsoleApplication/Program.cs
+++ ConsoleApplication/ConsoleApplication/Program.cs
@@ -1,20 +1,21 @@
 using System;
 using System.Linq;
+using ConsoleApplication;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Enter numbers separated by comma:");
+
         string input = Console.ReadLine();
-        Console.WriteLine("You entered: " + input);
-        
+
         int[] numbers = input.Split(',')
             .Select(int.Parse)
             .ToArray();
 
-        Console.WriteLine("Parsed " + numbers.Length + " numbers.");
-        
-        
+        int sum = StatisticsHelper.Sum(numbers);
+
+        Console.WriteLine("Sum: " + sum);
     }
 }
