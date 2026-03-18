6bffd1fbf8a0afc3a59cb78079bd10d035518195
Jakub Fudala <s33046@pjwstk.edu.pl>
2026-03-18 19:02:11 +0100

    Add CalculateMax method

--- ConsoleApplication/ConsoleApplication/StatisticsHelper.cs
+++ ConsoleApplication/ConsoleApplication/StatisticsHelper.cs
@@ -19,7 +19,7 @@ namespace ConsoleApplication
             return values.Average();
         }
         
-        public static int CalculateMaxx(int[] values)
+        public static int CalculateMax(int[] values)
         {
             return values.Max();
         }
